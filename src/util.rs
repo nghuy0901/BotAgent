7e1597cd261704f928553f8a19552ff388473fff
nghuy0901 <102844688+nghuy0901@users.noreply.github.com>
2026-05-23 09:21:12 +0200

    feat: more channel kinds for `channel_kind_to_value`

--- src/util.rs
+++ src/util.rs
@@ -3,10 +3,18 @@ use serenity::all::ChannelType;
 
 pub fn channel_kind_to_value(kind: ChannelType) -> Value {
     json!(match kind {
-        ChannelType::Text => "text",
         ChannelType::Category => "category",
-        ChannelType::Voice => "voice_chat",
+        ChannelType::Directory => "directory",
+        ChannelType::Forum => "forum",
+        ChannelType::GroupDm => "group_dm",
+        ChannelType::News => "news",
+        ChannelType::NewsThread => "news_thread",
+        ChannelType::Private => "direct_message",
+        ChannelType::PrivateThread => "private_thread",
+        ChannelType::PublicThread => "public_thread",
         ChannelType::Stage => "stage",
+        ChannelType::Text => "text",
+        ChannelType::Voice => "voice_chat",
         _ => "unknown",
     })
 }
